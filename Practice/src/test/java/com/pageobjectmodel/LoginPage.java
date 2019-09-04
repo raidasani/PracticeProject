c728cf8df092b71e7d49c4f8e298964a28a8a0c2
raida <raida@10.67.111.53>
2019-09-03 20:12:03 -0400

    hi

---
+++ Practice/src/test/java/com/pageobjectmodel/LoginPage.java
@@ -0,0 +1,31 @@
+package com.pageobjectmodel;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+WebDriver driver; 
+public LoginPage(WebDriver driver ) {
+	this.driver=driver;
+	PageFactory.initElements(driver, this);
+	
+}
+@FindBy(xpath="//input[@id='email']")
+WebElement login;
+public WebElement getLogin() {
+	return login;
+
+}
+
+@FindBy(xpath="//input[@id='pass']")
+ private WebElement password;
+private WebElement getPassword() {
+	return password;
+}
+
+
+
+
+}
